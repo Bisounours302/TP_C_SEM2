545a564be01861d802b3824ebefed518ff2a8620
Bisounours302 <124866425+Bisounours302@users.noreply.github.com>
2023-03-08 14:09:55 +0100

    14:09

--- tpBiblio.c
+++ tpBiblio.c
@@ -10,7 +10,7 @@ printf("\n 2 - afficher tous les livres de la bibliotheque ");
 printf("\n 3 - rechercher un livre (par son titre)");  // il peut y avoir plusieurs livres de même titre. Dans ce cas, indiquez le nombre d'exemplaires disponibles
 printf("\n 4 - rechercher et afficher tous les livres d'un auteur");
 printf("\n 5 - supprimer un livre de la bibliotheque");
-// si les 5 choix ci dessus sont bien codés, modifiez votre structure T_Livre et passez à 5 champs (avec cote,editeur et annee)
+// si les 5 choix ci dessus sont bien codés, modifiez votre structure T_Livre et passez à 5 champs (avec code,editeur et annee)
 
 
 // au programme du TP8 :
@@ -40,9 +40,16 @@ return choix;
 
 int main()
 {
+lectureFichierTXT();
 int reponse,chx;
 T_Bibliotheque B; 
 init( &B );
+T_Titre t;
+T_Aut aut;
+
+
+chargement(&B); //chargement d'un fichier de structure(s) T_livre
+
 
 do
 {
@@ -61,14 +68,39 @@ switch(chx)
 
 			break;	
 	
+	case 3 : 
+			lireChaine("TITRE :", t, MAX_TITRE );
+			reponse=AfficherLivresTitre(&B, t);
+			if (reponse==-1)	
+					printf("\nLa bibliotheque est vide\n");
+			if (reponse==0)	
+					printf("\nLivre non trouvé\n");
+			break;	
 	
+	case 4 :
+			lireChaine("AUTEUR :", aut, K_MaxAut );
+			reponse=AfficherLivresAuteur(&B, aut);
+			if (reponse==-1)	
+					printf("\nLa bibliotheque est vide\n");
+			if (reponse==0)	
+					printf("\nLivre non trouvé\n");
+			break;	
+	
+	case 5 :
+			lireChaine("TITRE :", t, MAX_TITRE );
+			reponse=SupprimerLivre(&B, t);
+			if (reponse==-1)	
+					printf("\nLa bibliotheque est vide\n");
+			if (reponse==0)	
+					printf("\nLivre non trouvé\n");
+			break;	
 	
 	
 	
 	}
 
 }while(chx!=0);
-
+sauvegarde(&B);
 
 
 
@@ -76,4 +108,82 @@ switch(chx)
 
 return 0;
 
+}
+
+void sauvegarde(T_Bibliotheque *ptrB)
+{
+FILE *fic=NULL; //le type FILE
+int i;
+fic=fopen("baseLivres","w"); // w = le mode = write avec ECRASEMENT
+//fopen renvoie NULL si probleme (disque plein, disque non accessible ...
+if (fic!=NULL)
+	{
+	for(i=0;i<ptrB->nbLivres;i++)
+		{
+//fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+    fwrite(  &(ptrB->etagere[i])        ,sizeof(T_livre),1,fic);
+
+		}
+	fclose(fic);
+	puts("SAUVEGARDE REUSSIE ..............");
+
+
+
+	}
+	else puts("ECHEC DE SAUVEGARDE  !!!!!  ");
+
+}
+
+
+
+void chargement(T_Bibliotheque *ptrB)
+{
+FILE *fic=NULL; //le type FILE
+int i=0;
+fic=fopen("baseLivres","r"); // r = le mode read
+//fopen renvoie NULL si probleme (disque plein, disque non accessible ...
+if (fic!=NULL)
+	{
+	do
+		{
+
+		fread(  &(ptrB->etagere[i]) ,sizeof(T_livre),1,fic);
+		i++;
+		}
+		while(!feof(fic));
+	fclose(fic);
+	ptrB->nbLivres=i-1;
+	puts("CHARGEMENT  REUSSI ..............");
+	}
+	else puts("ECHEC DE CHARGEMENT  !!!!!  ");
+
+}
+
+void lectureFichierTXT()
+{
+int M=100;
+FILE *fic=NULL; //le type FILE
+char chaine[M];
+char chaine2[M];
+char c;
+fic=fopen("fic.txt","rt"); // r = le mode read   w = mode write (avec ecrasement)
+//fopen renvoie NULL si probleme (fichier effac� , disque non accessible ...
+if (fic!=NULL)
+	{
+	do
+		{
+		//fgets(chaine,M,fic);   //fputs pour �crire dans un fichier txt
+		//fscanf(fic,"%s",chaine); //fprintf pour �crire dans un fichier txt
+	//	fscanf(fic,"%s %s",chaine,chaine2);
+		fscanf(fic,"%c",&c);
+		//fscanf(fic,"%c",&chaine[0]);
+	//	if (!feof(fic))
+           //printf("\n\t >%s--%s<",chaine,chaine2);
+        printf(">%c<",c);
+		}
+    while(!feof(fic));
+	fclose(fic);
+	puts("\nLECTURE REUSSIE ..............");
+	}
+	else puts("ECHEC DE LECTURE DU FICHIER TXT !!!!!  ");
 }
